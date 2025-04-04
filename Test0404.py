228be67a79cd479007b99c7328c94bdee59a6c55
Isabella Ponticelli <ponticelliisabella@gmail.com>
2025-04-04 11:23:33 +0200

    Update Test0404.py

--- Test0404.py
+++ Test0404.py
@@ -10,23 +10,29 @@ lista_bool = []
 
 def funz_tip():
     valore = input("Inserisci un valore di tipo int/str/bool: ")
-    if type(valore) == int:
-        lista_int.append(valore)
+    
+    if valore.isdigit(): #dato che dopo una serie di prove mi sono ricordata che un input 
+        #restituirà sempre una stringa, 
+        #ho trovato questo comando per verificare se il valore è un intero oppure no 
+        lista_int.append(int(valore))
         print("Valore {valore} inserito nella lista degli interi")
         print(lista_int)
-    elif type(valore) == str:
+#qui controllo se è un bool ma metto i valori tra virgolette per lo stesso motivo di prima
+    elif valore == "True" or valore == "False":
+        lista_bool.append(bool(valore))
+        print("Valore {valore} inserito nella lista dei booelani")
+        print(lista_bool)
+    else: # in questo caso meglio inserire questo controllo nell'else perchè se non è un bool o un int è per forza una stringa
         lista_str.append(valore)
         print("Valore {valore} inserito nella lista delle stringhe")
         print(lista_str)
-    elif type(valore)== bool:
-        lista_bool.append(valore)
-        print("Valore {valore} inserito nella lista dei booelani")
-        print(lista_bool)
-    else:
-        print("Valore inserito non valido")
         
 while True:
     funz_tip()
     scelta = input("Vuoi continuare? si/no: ")
     if scelta.lower() == "no":
+        print("Lista degli interi:", lista_int)
+        print("Lista delle stringhe:", lista_str)
+        print("Lista dei booleani:", lista_bool)
         break
+       
